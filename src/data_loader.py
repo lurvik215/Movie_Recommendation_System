c44178ed13e8eccb2f15a3ce86d5792f1645f799
Lurvik <pokalalurvik@gmail.com>
2026-03-09 00:09:06 +0530

    Initial project structure and EDA

---
+++ src/data_loader.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def load_movies(path="../DataSets/movies.csv"):
+    movies = pd.read_csv(path)
+    return movies
+
+
+def load_ratings(path="../DataSets/ratings.csv"):
+    ratings = pd.read_csv(path)
+    return ratings
+
+
+def merge_datasets(movies, ratings):
+    df = pd.merge(ratings, movies, on="movieId")
+    return df
+
+def save_dataset(df, path):
+    df.to_csv(path, index=False)
